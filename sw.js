8f75bf19d5aafcc13d86984a9000d94816f3cdb3
erunika7-crypto <erunika7@gmail.com>
2026-06-15 16:40:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "torisetsu-compass-v8";
+const CACHE_NAME = "torisetsu-compass-v9";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
